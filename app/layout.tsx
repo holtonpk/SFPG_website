acb2b4c119883fa49afb63ef80febbe38ee8895f
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-08-25 15:57:04 -0600

    removed inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { StorageProvider } from "@/context/storage";
 import { Toaster } from "@/components/ui/toaster";
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <StorageProvider>
-        <body className={inter.className}>
+        {/* <body className={inter.className}> */}
+        <body>
           <main>{children}</main>
           <Toaster />
         </body>
