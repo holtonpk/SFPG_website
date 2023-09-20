5edb54154770c2c267de2da373976c061a013239
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-09-19 22:44:16 -0600

    page description

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,5 +1,10 @@
 import Waitlist from "./waitlist";
+import { constructMetadata } from "@/lib/utils";
 
+export const metadata = constructMetadata({
+  title: "Sign up for the waitlist",
+  description: `Sign up for the waitlist and be the first to know when we launch "The 50 Greatest Business Success Stories"`,
+});
 const page = () => {
   return <Waitlist />;
 };
