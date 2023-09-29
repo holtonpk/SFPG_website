f8395b6539f3ad215028cde64b276a5426d218c5
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-09-29 17:56:27 -0600

    ga

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -15,5 +16,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
--- google-analytics.js
+++ google-analytics.js
@@ -1,5 +1,5 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
-  ReactGA.initialize("G-T175F5NDHX");
+  ReactGA.initialize("G-P361ZCLTZC");
 };
