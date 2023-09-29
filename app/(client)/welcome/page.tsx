8592865eaa4f7a731e0325094a8836857cf72407
Patrick Holton <patrickholton@Patricks-MacBook-Pro.local>
2023-09-29 17:58:36 -0600

    ga

--- app/(client)/welcome/page.tsx
+++ app/(client)/welcome/page.tsx
@@ -1,20 +1,8 @@
 "use client";
 import React from "react";
 import Waitlist from "./waitlist";
-import ReactGA from "react-ga";
-import { initGA } from "@/google-analytics.js";
 
 const Page = () => {
-  React.useEffect(() => {
-    initGA();
-  }, []);
-
-  // React.useEffect(() => {
-  //   if (window) {
-  //     ReactGA.pageview(window.location.pathname + window.location.search);
-  //   }
-  // }, [window]);
-
   return <Waitlist />;
 };
 
--- google-analytics.js
+++
@@ -1,5 +0,0 @@
-import ReactGA from "react-ga";
-
-export const initGA = () => {
-  ReactGA.initialize("G-P361ZCLTZC");
-};
